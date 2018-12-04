e73ffbdcff64c35ae3be3f94d0ee57635070fe1b
Andy Lindsay <andydlindsay@gmail.com>
2018-12-04 12:45:01 -0800

    completes exercise 5

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,25 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+surrey = Store.new(name: "Surrey", annual_revenue: 224_000, mens_apparel: false, womens_apparel: true)
+surrey.save
+
+whistler = Store.new(name: "Whistler", annual_revenue: 1_900_000, womens_apparel: false, mens_apparel: true)
+whistler.save
+
+yaletown = Store.new(name: "Yaletown", annual_revenue: 430_000, mens_apparel: true, womens_apparel: true)
+yaletown.save
+
+@mens_stores = Store.where(mens_apparel: true)
+
+puts "----- Mens Stores Only -----"
+@mens_stores.each do |store|
+    puts "Store name: #{store.name}, Annual revenue: #{store.annual_revenue}"
+end
+
+puts "----- Womens stores with less than $1M annual sales -----"
+@womens_stores = Store.where(womens_apparel: true).where('annual_revenue < ?', 1_000_000)
+
+@womens_stores.each do |store|
+    puts "Store name: #{store.name}, Annual revenue: #{store.annual_revenue}"
+end
--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,11 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+@annual_revenue = Store.sum(:annual_revenue)
+puts "Annual revenue: #{@annual_revenue}"
+
+@average_revenue = @annual_revenue / Store.count
+puts "Average revenue: #{@average_revenue}"
+
+@stores_making_gt_1M = Store.where('annual_revenue >= ?', 1_000_000).count
+puts "Number of stores making 1M or more: #{@stores_making_gt_1M}"
